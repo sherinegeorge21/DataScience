20f90c11224fa563783d6735934a3b2b26323efc
sherinegeorge21 <40655116+sherinegeorge21@users.noreply.github.com>
2023-10-11 13:59:55 -0400

    Update app.py

--- heatwaves_streamlit/app.py
+++ heatwaves_streamlit/app.py
@@ -1,7 +1,4 @@
-# @Email:  contact@pythonandvba.com
-# @Website:  https://pythonandvba.com
-# @YouTube:  https://youtube.com/c/CodingIsFun
-# @Project:  Sales Dashboard w/ Streamlit
+# @Project:  Heatwaves Dashboard w/ Streamlit
 
 
 
@@ -54,59 +51,6 @@ st.markdown("##")
 
 st.dataframe(df_selection)
 
-# # TOP KPI's
-# total_sales = int(df_selection["Heatwaves_WMO"].sum())
-# average_rating = round(int(df_selection["Heatwaves_WMO"].mean(), 1))
-# star_rating = ":star:" * int(round(average_rating, 0))
-# #average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
-
-# left_column, middle_column = st.columns(2)
-# with left_column:
-#     st.subheader("Total Sales:")
-#     st.subheader(f"US $ {total_sales:,}")
-# with middle_column:
-#     st.subheader("Average Rating:")
-#     st.subheader(f"{average_rating} {star_rating}")
-
-
-# st.markdown("""---""")
-
-# # SALES BY PRODUCT LINE [BAR CHART]
-# sales_by_product_line = df_selection.groupby(by=["CountryName"])[["Heatwaves_WMO"]].sum().sort_values(by="Heatwaves_WMO")
-# fig_product_sales = px.bar(
-#     sales_by_product_line,
-#     x="Total",
-#     y=sales_by_product_line.index,
-#     orientation="h",
-#     title="<b>Sales by Product Line</b>",
-#     color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-#     template="plotly_white",
-# )
-# fig_product_sales.update_layout(
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     xaxis=(dict(showgrid=False))
-# )
-
-# # SALES BY HOUR [BAR CHART]
-# # sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum()
-# # fig_hourly_sales = px.bar(
-# #     sales_by_hour,
-# #     x=sales_by_hour.index,
-# #     y="Total",
-# #     title="<b>Sales by hour</b>",
-# #     color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
-# #     template="plotly_white",
-# # )
-# # fig_hourly_sales.update_layout(
-# #     xaxis=dict(tickmode="linear"),
-# #     plot_bgcolor="rgba(0,0,0,0)",
-# #     yaxis=(dict(showgrid=False)),
-# # )
-
-
-# left_column, right_column = st.columns(2)
-# left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-# right_column.plotly_chart(fig_product_sales, use_container_width=True)
 
 
 # ---- HIDE STREAMLIT STYLE ----
